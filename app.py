6a6dfd01ad205d0c63cb70556f719713f1660aa5
nebuss <subin272051@gmail.com>
2024-11-01 09:43:49 +0900

    1101

--- app.py
+++ app.py
@@ -4,8 +4,47 @@ import snowflake.connector
 import streamlit_option_menu
 from streamlit_option_menu import option_menu
 
-!pip install streamlit
+# !pip install streamlit
+# !pip install snowflake-connector-python
+# !pip install streamlit_option_menu
 
+# %%writefile app.py
+
+from PIL import Image
+
+st.write('Hello, *World!* :sunglasses:') # 해당 내용을 수정해서 사이트를 자유롭게 꾸밀 수 있다.
+
+st.title('this is title')
+st.header('this is header')
+st.subheader('this is subheader')
+
+# 탭 생성 : 첫번째 탭의 이름은 Tab A 로, Tab B로 표시합니다.
+tab1, tab2= st.tabs(['Tab A' , 'Tab B'])
+
+with tab1:
+  #tab A 를 누르면 표시될 내용
+  st.write('hello')
+
+with tab2:
+  #tab B를 누르면 표시될 내용
+  st.write('hi')
+
+# 데이터 프레임
+import pandas as pd
+df = pd.DataFrame({
+     '첫 번째 컬럼': [1, 2, 3, 4],
+     '두 번째 컬럼': [10, 20, 30, 40]
+     })
+st.write(df)
+
+
+
+import urllib
+print("Password/Enpoint IP for localtunnel is:",urllib.request.urlopen('https://ipv4.icanhazip.com').read().decode('utf8').strip("\n"))
+
+# !npm install localtunnel
+# !streamlit run app.py &>/content/logs.txt &
+# !npx localtunnel --port 8501
 with st.sidebar:
   selected = option_menu(
     menu_title = "Main Menu",
@@ -36,3 +75,4 @@ with st.sidebar:
     st.title(f"You Have selected {selected}")
   if selected == "Contact":
     st.title(f"You Have selected {selected}")
+# %%
