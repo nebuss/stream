f283ac07a6f54e6b2a537eb53e47f65b6315f363
nebuss <subin272051@gmail.com>
2024-11-01 10:16:50 +0900

    1101

--- app.py
+++ app.py
@@ -1,17 +1,7 @@
+
 import streamlit as st
-import pandas as pd
-import snowflake.connector
-import streamlit_option_menu
-from streamlit_option_menu import option_menu
-
-# !pip install streamlit
-# !pip install snowflake-connector-python
-# !pip install streamlit_option_menu
-
-# %%writefile app.py
-
 from PIL import Image
-
+import urllib.request
 st.write('Hello, *World!* :sunglasses:') # 해당 내용을 수정해서 사이트를 자유롭게 꾸밀 수 있다.
 
 st.title('this is title')
@@ -38,41 +28,10 @@ df = pd.DataFrame({
 st.write(df)
 
 
-
 import urllib
 print("Password/Enpoint IP for localtunnel is:",urllib.request.urlopen('https://ipv4.icanhazip.com').read().decode('utf8').strip("\n"))
+
 
 # !npm install localtunnel
 # !streamlit run app.py &>/content/logs.txt &
 # !npx localtunnel --port 8501
-with st.sidebar:
-  selected = option_menu(
-    menu_title = "Main Menu",
-    options = ["Home","Projects","Contact"],
-    icons = ["house","book","envelope"],
-    menu_icon = "cast",
-    default_index = 0,
-
-  )
-  if selected == "Home":
-    st.title(f"You Have selected {selected}")
-    st.header('Snowflake Healthcare App')
-    my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
-    my_cur = my_cnx.cursor()
-    # run a snowflake query and put it all in a var called my_catalog
-    my_cur.execute("select * from SWEATSUITS")
-    my_catalog = my_cur.fetchall()
-    st.dataframe(my_catalog)
-    q1 = st.text_input('Write your query','')
-    st.button('Run Query')
-    if not q1:
-      st.error('Please write a query')
-    else:
-      my_cur.execute(q1)
-      my_catalog = my_cur.fetchall()
-      st.dataframe(my_catalog)
-  if selected == "Projects":
-    st.title(f"You Have selected {selected}")
-  if selected == "Contact":
-    st.title(f"You Have selected {selected}")
-# %%
